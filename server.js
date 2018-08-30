52f4d517cd448afd32c1f11d91fcdf5ef822a847
ashwin <ashwin.ashwinkhanna@gmail.com>
2018-08-30 22:55:43 +0000

    first commit, api call works and displays price info of currency using axios

---
+++ server.js
@@ -0,0 +1,18 @@
+const express     = require('express');
+const app         = express();
+const axios       = require('axios')
+const port        = 8080;
+const env         = require('dotenv').config()
+const client      = process.env.API_KEY;
+
+
+axios.get('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR')
+  .then(function (res) {
+    console.log(res.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
